0045cd48e296afe54ae1ba78eb145a31d3fa7fee
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-15 17:42:13 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by Kusunose Hosho on 2020/05/15.
+//  Copyright © 2020 Kusunose Hosho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
